216fe1171b94e1d1acd22e1b02c08b8cf630de4f
dilin1212 <dilindinesan@gmail.com>
2017-05-09 21:39:47 +0300

    Add project files.

---
+++ Diamondpgm/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamondpgm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                for (int i = 15; i >= 0; i--)
+                {
+                    for (int n = 0; n < 15; n++)
+                    {
+                        if (i > n)
+                            Console.Write(" ");
+                        else
+                            Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
